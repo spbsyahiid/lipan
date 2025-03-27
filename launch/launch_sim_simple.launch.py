5c158c7d30837a737e6f9a5598157575378e54cb
Syahiid Rasidi <syahiid.rasidi@gmail.com>
2025-03-27 09:48:10 +0800

    gz sim instead of classic gazebo

--- launch/launch_sim_simple.launch.py
+++ launch/launch_sim_simple.launch.py
@@ -1,24 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    world_path = os.path.join(
+        get_package_share_directory('articubot_one'),
+        'worlds',
+        'obstacles.world'
+    )
+
+    robot_description_path = os.path.join(
+        get_package_share_directory('articubot_one'),
+        'urdf',
+        'articubot.urdf'
+    )
+
     return LaunchDescription([
+        Node(
+            package='ros_gz_sim',
+            executable='gz_sim',
+            arguments=['-r', world_path],
+            output='screen'
+        ),
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=['-name', 'my_bot', '-file', robot_description_path],
+            output='screen'
+        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': True}],
-            arguments=['$(find-pkg-share articubot_one)/urdf/articubot_one.urdf'],
-        ),
-        Node(
-            package='gazebo_ros',
-            executable='gzserver',
-            output='screen',
-            arguments=['-s', 'libgazebo_ros_factory.so'],
-        ),
-        Node(
-            package='gazebo_ros',
-            executable='gzclient',
-            output='screen',
+            parameters=[{'use_sim_time': True, 'robot_description': open(robot_description_path).read()}],
+            output='screen'
         )
     ])
