a4a03c5990583b739f42562e897e4b31e7194b35
Syahiid Rasidi <syahiid.rasidi@gmail.com>
2025-04-07 09:09:31 +0800

    Ladang.sdf

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
-        'empty.world'
+        'ladang.sdf'
         )    
     
     world = LaunchConfiguration('world')
